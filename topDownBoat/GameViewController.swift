e8b31f8b3e609c7a4cf55683b08378de6de32cfa
José Henrique Fernandes Silva <jhfs@cin.ufpe.br>
2021-06-18 12:04:40 -0300

    Adiciona projeto base

---
+++ topDownBoat/GameViewController.swift
@@ -0,0 +1,34 @@
+//
+//  GameViewController.swift
+//  topDownBoat
+//
+//  Created by José Henrique Fernandes Silva on 18/06/21.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+}
